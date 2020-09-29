a5d8b5cce061adaeae93bf81d538603831c674c5
Raattis <ihmisraato@hotmail.com>
2020-09-30 02:32:11 +0300

    Clean up chat function.

--- server/src/chat.ts
+++ server/src/chat.ts
@@ -4,19 +4,18 @@ import { queueSaving, readChatState } from './state_saving'
 import { log, getLogMessages } from './log'
 import { app } from './app'
 
-let portNumber = 0;
-
-let chatState : ChatState = new ChatState();
-
 export const initChat = async (listeningToPort : number) =>
 {
-    portNumber = listeningToPort;
+    const portNumber = listeningToPort;
 
+    let chatState : ChatState = new ChatState();
     try
     {
         let result : ChatState | undefined = await readChatState();
-        if (result)
+        if (result){
             chatState = result;
+            log("Chat state loaded.");
+        }
     }
     catch
     {
@@ -30,17 +29,17 @@ export const initChat = async (listeningToPort : number) =>
     }
     
     const joinChannel = (join : JoinChannel) => {
-        if (!chatState.users.find(u => u.name == join.user_name)) {
+        if (!chatState.users.find(u => u.name === join.user_name)) {
             chatState.users.push({name:join.user_name, ping:new Date()});
         }
 
-        if (!chatState.channels.find(c => c.name == join.channel_name)){
+        if (!chatState.channels.find(c => c.name === join.channel_name)){
             let channel:Channel = new Channel();
             channel.name = join.channel_name;
             chatState.channels.push(channel);
         }
 
-        chatState.channels.filter(c => c.name == join.channel_name).forEach(c => {if (!c.user_names.find(u => u == join.user_name)) c.user_names.push(join.user_name)});
+        chatState.channels.filter(c => c.name === join.channel_name).forEach(c => {if (!c.user_names.find(u => u === join.user_name)) c.user_names.push(join.user_name)});
         queueSaving(chatState);
         log("User " + join.user_name + " joined " + join.channel_name + "");
     }
@@ -53,22 +52,18 @@ export const initChat = async (listeningToPort : number) =>
     }
     
     const sendMessage = (msg : ChannelMessage) => {
-        chatState.channels.filter(c => c.name == msg.channel_name).forEach(c => c.messages.push({sender_name:msg.sender_name, msg:msg.message, date:new Date()}));
+        chatState.channels.filter(c => c.name === msg.channel_name).forEach(c => c.messages.push({sender_name:msg.sender_name, msg:msg.message, date:new Date()}));
         queueSaving(chatState);
         log("User " + msg.sender_name + " sent message " + msg.message + " to " + msg.channel_name + "");
     }
 
     const ping = (user_name : string)=>{
-        let user =  chatState.users.find(u => u.name == user_name);
+        let user =  chatState.users.find(u => u.name === user_name);
         if (user){
             user.ping = new Date();
         }
     }
     
-    app.get('/', (req, res) => {
-        res.send(getLogPage());
-    });
-
     app.post('/join', (req, res) => {
         const join: JoinChannel = req.body;
         joinChannel(join);
@@ -110,29 +105,33 @@ export const initChat = async (listeningToPort : number) =>
         queueSaving(chatState);
         res.send(JSON.stringify(chatState));
     });
-}
 
-const getLogPage = () => {
-    const escapeHtml = (s: string) => {
-        return s
-            .replace(/&/g, "&amp;")
-            .replace(/</g, "&lt;")
-            .replace(/>/g, "&gt;")
-            .replace(/"/g, "&quot;")
-            .replace(/'/g, "&#039;");
+    const getLogPage = () => {
+        const escapeHtml = (s: string) => {
+            return s
+                .replace(/&/g, "&amp;")
+                .replace(/</g, "&lt;")
+                .replace(/>/g, "&gt;")
+                .replace(/"/g, "&quot;")
+                .replace(/'/g, "&#039;");
+        }
+
+        let output = "";
+        output += `<!DOCTYPE html>\n<html>\n<head><title>server@${portNumber}</title></head>\n`;
+        output += `<body><h1>Server running on port ${portNumber}</h1>\n`;
+        output += `<p>Connect with the <b>shlock_client</b></p>\n`;
+        output += `<p>Logs:</p><table><tr><th>Time</th><th>Message</th></tr>\n`;
+
+        getLogMessages().slice(-10).reverse().forEach(e => {
+            output += `<tr><td><b>[${e.t.toISOString().replace('T', ' ').slice(0, -5)}]</b></td><td>`;
+            output += `${escapeHtml(e.m)}</td></tr>\n`
+        });
+        output += "</table>\n</body>\n</html>";
+
+        return output;
     }
 
-    let output = "";
-    output += `<!DOCTYPE html>\n<html>\n<head><title>server@${portNumber}</title></head>\n`;
-    output += `<body><h1>Server running on port ${portNumber}</h1>\n`;
-    output += `<p>Connect with the <b>shlock_client</b></p>\n`;
-    output += `<p>Logs:</p><table><tr><th>Time</th><th>Message</th></tr>\n`;
-
-    getLogMessages().slice(-10).reverse().forEach(e => {
-        output += `<tr><td><b>[${e.t.toISOString().replace('T', ' ').slice(0, -5)}]</b></td><td>`;
-        output += `${escapeHtml(e.m)}</td></tr>\n`
+    app.get('/', (req, res) => {
+        res.send(getLogPage());
     });
-    output += "</table>\n</body>\n</html>";
-
-    return output;
 }
